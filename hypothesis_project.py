ce2653f5edeaee6e9f305cecde4ecf2b0375e1e6
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2021-02-22 12:25:16 +0200

    added project 3 to hypothesis_project.py

--- hypothesis_project.py
+++ hypothesis_project.py
@@ -79,3 +79,53 @@ print(Xtab)
 
 chi2, pval, dof, exp = scipy.chi2_contingency(Xtab)
 print(pval)
+
+
+# Project 3
+
+# Read in the `clicks.csv` file as `abdata`
+abdata = pd.read_csv('clicks.csv')
+print(abdata.head())
+
+Xtab = pd.crosstab(abdata.group, abdata.is_purchase)
+print(Xtab)
+
+chi2, pval, dof, exp = scipy.chi2_contingency(Xtab)
+print(pval)
+
+num_visits = len(abdata)
+
+num_sales_needed_099 = 1000 / 0.99
+
+p_sales_needed_099 = num_sales_needed_099 / num_visits
+
+num_sales_needed_199 = 1000 / 1.99
+
+p_sales_needed_199 = num_sales_needed_199 / num_visits
+
+num_sales_needed_499 = 1000 / 4.99
+
+p_sales_needed_499 = num_sales_needed_499 / num_visits
+
+print(p_sales_needed_099, p_sales_needed_199, p_sales_needed_499)
+
+samp_size_099 = np.sum(abdata.group == 'A')
+sales_099 = np.sum((abdata.group == 'A') & (abdata.is_purchase == 'Yes'))
+
+samp_size_199 = np.sum(abdata.group == 'B')
+sales_199 = np.sum((abdata.group == 'B') & (abdata.is_purchase == 'Yes'))
+
+samp_size_499 = np.sum(abdata.group == 'C')
+sales_499 = np.sum((abdata.group == 'C') & (abdata.is_purchase == 'Yes'))
+
+
+pvalueA = scipy.binom_test(sales_099, samp_size_099,
+                           p_sales_needed_099, alternative='greater')
+
+pvalueB = scipy.binom_test(sales_199, samp_size_199,
+                           p_sales_needed_199, alternative='greater')
+
+pvalueC = scipy.binom_test(sales_499, samp_size_499,
+                           p_sales_needed_499, alternative='greater')
+
+print(pvalueA, pvalueB, pvalueC)

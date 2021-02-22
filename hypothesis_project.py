dc9a8e870994b0a131698881034b30c9dcc4041e
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2021-02-22 12:10:14 +0200

    added project 2 to hypothesis_project.py

--- hypothesis_project.py
+++ hypothesis_project.py
@@ -1,6 +1,8 @@
 # Project 1
 
 # Import libraries
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+import codecademylib3
 import pandas as pd
 import numpy as np
 import scipy.stats as scipy
@@ -9,7 +11,7 @@ import scipy.stats as scipy
 lifespans = pd.read_csv('familiar_lifespan.csv')
 iron = pd.read_csv('familiar_iron.csv')
 
-#inspect data
+# inspect data
 print(lifespans.head())
 
 vein_pack_lifespans = lifespans.lifespan[lifespans.pack == 'vein']
@@ -35,5 +37,45 @@ Xtab = pd.crosstab(iron.pack, iron.iron)
 print(Xtab)
 
 # chi2 test
+chi2, pval, dof, exp = scipy.chi2_contingency(Xtab)
+print(pval)
+
+
+# Project 2
+# Import libraries
+
+# Import data
+dogs = pd.read_csv('dog_data.csv')
+print(dogs.head())
+
+# Subset to just whippets, terriers, and pitbulls
+dogs_wtp = dogs[dogs.breed.isin(['whippet', 'terrier', 'pitbull'])]
+
+# Subset to just poodles and shihtzus
+dogs_ps = dogs[dogs.breed.isin(['poodle', 'shihtzu'])]
+
+whippet_rescue = dogs.is_rescue[dogs.breed == 'whippet']
+num_whippet_rescues = np.sum(whippet_rescue)
+print(num_whippet_rescues)
+
+num_whippets = len(whippet_rescue)
+print(num_whippets)
+
+pval = scipy.binom_test(num_whippet_rescues, num_whippets, 0.08)
+print(pval)
+
+wt_whippets = dogs.weight[dogs.breed == 'whippet']
+wt_terriers = dogs.weight[dogs.breed == 'terrier']
+wt_pitbulls = dogs.weight[dogs.breed == 'pitbull']
+
+Fstat, pval = scipy.f_oneway(wt_whippets, wt_terriers, wt_pitbulls)
+print(pval)
+
+output = pairwise_tukeyhsd(dogs_wtp.weight, dogs_wtp.breed)
+print(output)
+
+Xtab = pd.crosstab(dogs_ps.color, dogs_ps.breed)
+print(Xtab)
+
 chi2, pval, dof, exp = scipy.chi2_contingency(Xtab)
 print(pval)

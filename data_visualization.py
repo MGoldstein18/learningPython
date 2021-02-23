cd8e313278085aafbe1415d3a243a05d9f86222e
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2021-02-23 11:48:11 +0200

    added another visualization

--- data_visualization.py
+++ data_visualization.py
@@ -178,3 +178,20 @@ plt.ylabel('Frequency')
 plt.savefig('my_histogram.png')
 
 plt.show()
+
+
+# Visualization 7
+
+
+unit_topics = ['Limits', 'Derivatives', 'Integrals', 'Diff Eq', 'Applications']
+num_hardest_reported = [1, 3, 10, 15, 1]
+
+plt.figure(figsize=(10, 8))
+plt.pie(num_hardest_reported, labels=unit_topics, autopct='%d%%')
+plt.axis('equal')
+
+plt.title('Hardest Topics')
+
+plt.savefig('my_pie_chart.png')
+
+plt.show()

50c2db979145af4ab57d7773543977b28e269c22
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2021-03-09 16:56:39 +0200

    updated bayes_theorum.py

--- bayes_theorum.py
+++ bayes_theorum.py
@@ -7,4 +7,4 @@ p_correct_given_knows = 0.85
 p_anyone_correct = (0.6 * 0.85) + (0.4 * 0.2)
 
 p_knows_given_correct = (0.85 * 0.6) / p_anyone_correct
-print p_knows_given_correct
+print (p_knows_given_correct)

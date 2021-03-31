1f52beac90435c6129d9819fa5f44173b1759838
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2021-03-31 21:17:59 +0200

    added new function to python_practice.py

--- python_practice.py
+++ python_practice.py
@@ -4,3 +4,13 @@ def nba_extrap(ppg, mpg):
     ppg = round((48 / mpg) * ppg, 1)
     return ppg
 
+# function to take the numerical position of a month and return the quarter of the year it falls into. For example: month 2 (February) returns 1
+def quarter_of(month):
+    if month <= 3:
+        return 1
+    elif month <= 6:
+        return 2
+    elif month <= 9:
+        return 3
+    else:
+        return 4
